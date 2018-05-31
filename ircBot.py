6563327db62f9414ebf09b3092d248f045617b99
AssayUK <38913184+AssayUK@users.noreply.github.com>
2018-05-31 16:59:04 +0100

    Update ircBot.py

--- ircBot.py
+++ ircBot.py
@@ -129,7 +129,7 @@ while True:
 		if count == 5:
 			inChan = True
 		time.sleep(5)
-		rawSend(":"  +ircNick +  " PRIVMSG " + " nickserv :identify " +ircPassword  + " \r\n")	
+		rawSend((":"  +ircNick +  " PRIVMSG " + " nickserv :identify " + ircPassword + " \r\n"))	
 		for channel in channelList:
 		    rawSend("JOIN " + channel + "\r\n")
                 rawSend("MODE " + ircNick + " +B" + "\r\n")
